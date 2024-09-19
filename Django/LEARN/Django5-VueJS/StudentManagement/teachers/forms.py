1a89879264cc0095095f3671cfdbf6b60faeacc6
H471967 <Runsheng.Wu@Honeywell.com>
2024-09-19 15:32:21 +0800

    update student_management

--- Django/LEARN/Django5-VueJS/StudentManagement/teachers/forms.py
+++ Django/LEARN/Django5-VueJS/StudentManagement/teachers/forms.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from datetime import datetime
+import datetime 
 from django import forms
 from .models import Teacher
 from grades.models import Grade
@@ -10,28 +10,28 @@ class TeacherForm(forms.ModelForm):
         model = Teacher
         fields = ['teacher_name', 'phone_number', 'gender', 'birthday', 'grade']
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     # 为前端表单重新定义 grade的排序方式
-    #     # 重新定义 外键 grade 为 Grade 的 object 的排序方式，并以 grade_number 进行排序（默认升序）
-    #     # self.fields.get('grade').queryset = Grade.objects.all().order_by('grade_number')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 为前端表单重新定义 grade的排序方式
+        # 重新定义 外键 grade 为 Grade 的 object 的排序方式，并以 grade_number 进行排序（默认升序）
+        self.fields.get('grade').queryset = Grade.objects.all().order_by('grade_number')
 
-    # def clean_teacher_name(self):
-    #     teacher_name = self.cleaned_data.get('teacher_name')
-    #     if len(teacher_name) < 2 or len(teacher_name) > 50:
-    #         raise ValidationError('请填写正确的姓名')
-    #     return teacher_name
+    def clean_teacher_name(self):
+        teacher_name = self.cleaned_data.get('teacher_name')
+        if len(teacher_name) < 2 or len(teacher_name) > 50:
+            raise ValidationError('请填写正确的姓名')
+        return teacher_name
 
-    # def clean_birthday(self):
-    #     birthday = self.cleaned_data.get('birthday')
-    #     if not isinstance(birthday, datetime.date):
-    #         raise ValidationError('生日日期格式错误，正确格式为如：2024-05-01')
-    #     if birthday > datetime.date.today():
-    #         raise ValidationError('生日日期不能大于今天')
-    #     return birthday
+    def clean_birthday(self):
+        birthday = self.cleaned_data.get('birthday')
+        if not isinstance(birthday, datetime.date):
+            raise ValidationError('生日日期格式错误，正确格式为如：2024-05-01')
+        if birthday > datetime.date.today():
+            raise ValidationError('生日日期不能大于今天')
+        return birthday
 
-    # def clean_phone_number(self):
-    #     phone_number = self.cleaned_data.get('phone_number')
-    #     if len(phone_number) != 11:
-    #         raise ValidationError('请填写正确的手机号（长度应为11位）')
-    #     return phone_number
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        if len(phone_number) != 11:
+            raise ValidationError('请填写正确的手机号（长度应为11位）')
+        return phone_number
--- Django/LEARN/Django5-VueJS/StudentManagement/teachers/views.py
+++ Django/LEARN/Django5-VueJS/StudentManagement/teachers/views.py
@@ -1,8 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Teacher
+from grades.models import Grade
 from .forms import TeacherForm
-from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -20,6 +24,22 @@ class TeacherListView(BaseTeacherView, ListView):
     paginate_by = 10
 
     # 3.搜索功能：基于特定字段过滤数据
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['grades'] = Grade.objects.all().order_by('grade_number')
+        context['current_grade'] = self.request.GET.get('grade', '')
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('search')
+        grade_id = self.request.GET.get('grade')
+        if grade_id:
+            queryset = queryset.filter(grade_id__icontains=grade_id)
+        if keyword:
+            filter = Q(teacher_name__icontains=keyword) | Q(phone_number__icontains=keyword)
+            queryset = queryset.filter(filter)
+        return queryset
 
     # 4.排序：基于特定字段的特定规则进行排序
 
@@ -29,7 +49,38 @@ class TeacherCreateView(BaseTeacherView, CreateView):
     template_name = "teachers/teacher_form.html"
     form_class = TeacherForm
 
+    # 接受form表单数据并创建对应用户 （字段验证发生在这之前，在 form 的clean_<field_data>中进行
+    def form_valid(self, form):
+        # 接受字段
+        teacher_name = form.cleaned_data.get('teacher_name')
+        phone_number = form.cleaned_data.get('phone_number')
+        # 创建对应用户
+        username = f'{teacher_name}_{phone_number}'
+        password = phone_number[-6:]
+        # 确保带创建用户不存在，否则直接使用已存在的同名用户
+        users = User.objects.filter(username=username)
+        if users.exists():
+            user = users.first()
+        else:
+            user = User.objects.create_user(username=username, password=password)
+        # 将用户写入 teacher, 模型实例关联
+        form.instance.user = user
+        form.save()
 
+        return JsonResponse({
+            'status': 'success',
+            'messages': '操作成功'
+        }, status=200)
+
+    def form_invalid(self, form):
+        errors = form.errors.as_json()
+        print(errors)
+        return JsonResponse({
+            'status': 'error',
+            'messages': errors
+        }, status=400)
+    
+    
 class TeacherUpdateView(BaseTeacherView, UpdateView):
     template_name = "teachers/teacher_form.html"
 
