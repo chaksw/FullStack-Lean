3ffff3a6362fb1ea1bb45dc67a7b57ed75223f00
H471967 <Runsheng.Wu@Honeywell.com>
2024-11-18 16:31:19 +0800

    update vue practice

--- JavaScript/PRACTICE/Front-End/Vue-Practice/scga_demo/vite.config.js
+++ JavaScript/PRACTICE/Front-End/Vue-Practice/scga_demo/vite.config.js
@@ -10,4 +10,15 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "src"),
 		},
 	},
+	server: {
+		proxy: {
+			"/api/": {
+				target: `http://127.0.0.1:8000/`, // 后端 API 服务器地址
+				changeOrigin: true, // 改变请求的源头，解决跨域问题
+				pathRewrite: {
+					"^/api": "", // 将 "/api" 前缀移除
+				},
+			},
+		},
+	},
 });
